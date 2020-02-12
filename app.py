d34d1a4dfda26a89bcb9618fabd85acab88e7a06
priyank.bhuva <priyank.bhuva@crestdatasys.com>
2020-02-12 18:35:57 +0530

    Completed Generating Access token. Database host,password,user provided in code.

--- app.py
+++ app.py
@@ -1,26 +1,70 @@
 import hashlib
+import time,datetime
+import random,json
 
-from flask import Flask
+from flask import Flask, Response
 from flask_httpauth import HTTPBasicAuth, HTTPTokenAuth
 
-app = Flask(__name__)
+import mysql.connector as mysql
 
+app = Flask(__name__)
 basic_auth = HTTPBasicAuth()
 token_auth = HTTPTokenAuth()
 
+config = {
+  'user': 'trustar',
+  'password': 'trustar',
+  'host': '107.23.198.222',
+  'database': 'trustar'
+}
+
+try:
+    connector = mysql.connect(**config)
+except mysql.Error as e:
+    print(e)
+
+# connector.close()
 
 @basic_auth.verify_password
 def verify_password(username, password):
-    print(username)
-    print(password)
-    return True
-
+    cursor=connector.cursor(buffered=True)
+    cursor.execute(f"SELECT count(*) FROM users WHERE username=\"{username}\" and password=\"{password}\"")
+    count = cursor.fetchone()[0]
+    cursor.close()
+    if count:
+        return True
+    else:
+        return False
 
 @app.route('/auth', methods=["GET"])
 @basic_auth.login_required
 def token_provider():
-    return hashlib.md5(bytes(basic_auth.username()))
+    ts = time.time()
+    timestamp = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+    token = hashlib.md5((basic_auth.username()+timestamp).encode())
+    token = token.hexdigest()
 
+    cursor = connector.cursor(buffered=True)
+    cursor.execute(f"SELECT token,time_stamp FROM users WHERE username=\"{basic_auth.username()}\"")
+
+    fetch_data = cursor.fetchone()
+    old_time = fetch_data[1].timestamp()
+    print(ts-old_time)
+    if ts - old_time >600:
+        cursor.reset()
+        cursor.execute(f"UPDATE users SET token=\"{token}\", time_stamp=\"{timestamp}\" WHERE username=\"{basic_auth.username()}\"")
+        connector.commit()
+    else:
+        token=fetch_data[0]
+
+    cursor.close()
+
+    response_json = {
+        "access_token":token
+    }
+    response = Response(json.dumps(response_json))
+    response.headers["Content-Type"] = "application/json"
+    return response
 
 @token_auth.verify_token
 def verify_token(token):
